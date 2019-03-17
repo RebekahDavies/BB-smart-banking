b6d3538c286e7021c128f84a2c42797fd58aa44c
Rebekah Davies <DaviesR130@cardiff.ac.uk>
2019-03-17 19:39:19 +0000

    Completed minimising overdraft fees
    Overdrafe fees functions have been completed and results haave been parsed back into a new csv file

--- README.md
+++ README.md
@@ -4,7 +4,17 @@ A smart banking app feature that will minimise overdraft fees by automatically t
 Instructions
 Before starting please make sure you have nodejs installed, you can download the windows exe here: http://nodejs.org/#download
 
-Next open up your terminal and cd into the project file BB-smart-banking. Install Papa Parse by entering the command 'npm install papaparse' into the console
+Next open up your terminal and cd into the project file BB-smart-banking. Install Papa Parse by entering the command 'npm install papaparse' into the console.
+
+After installation is complete, to run the app enter 'Node fee-minimiser' into the console.
+
+After entering the command you should see a print out of the transactions before any minimisations. 
+After this you should see the log 'minimising overdraft fees.......'.
+After this log is a print out of the transactions, including the new ones transfering money to the savings
+
+You will also find a new file in the project called customer-1234567-ledger-new.csv. 
+
+This should also work with other ledgers of the same structure and fileds. To use a different file adjust the file names and paths if needed on lines 4 and 5
 
 
 Dev thought process
@@ -13,9 +23,22 @@ In this secton I will record my assumptions and though process whilst developing
 
 For this task I have chosen to use JavaScript due to my preference to front end development. I will also be uising Node.js File parser module along with th csv parser 'Papa Parse'
 
-I will go about this task in an iterative way, starting small and building upon it. The first thing I will do it to access the csv file line by line and print to the console. 
+I will go about this task in an iterative way, starting small and building upon it. The first thing I will do it to access the csv file line by line and print to the console.
+
+After the first commit, it now parses the file. the file is located by creating a read stream using the NodeJS module FS. The CSV file is then parsed using Papa Parse. The parse function takes in two paramenters, the file to parse and then a config object. Inside the config is where I set the headers to true which tells the parse that the CSV file has a header row. This then changes the result data into objects rather than just an array. Also in the config is where you can dictate what happens after every row of data and when the parsing is complete. 
+I have set it so that after every row, that row or record of data is turned into a transaction object. 
+
+The next I will create the minimiser function, which will iterate through each record object, and create two transactions every time the current account balance goes below 0 to transfer the money from savings, as long as there is sufficient funds in the savings. 
+
+To complete the overdraft fee minimisation I first needed to iterate through each transaction record and record the totals for both the savings and the current account. Thankfully JavaScript can deal with negative numbers whilst using them in sums so there was no need for me to get rid of the minus sign. 
+After I had a running total going for each account, I then put in a conditional system so that when any amount of money is taken out of the current account, if the customer goes into their overdraft and if there is enough money in the savings account, then the needed amount of money to put the current account balance to £0, and add in the two transaction records into the positions in front of that record. 
+
+I was then able to convert the transactions array into JSON and parse that back to a csv file. 
 
 
 Links Used: 
 https://stackoverflow.com/questions/6737824/how-to-run-a-hello-js-file-in-node-js-on-windows/6738741
-https://gooroo.io/GoorooTHINK/Article/17328/How-to-read-and-parse-a-CSV-file-using-NodeJS/27993#.XIzq1Sj7TIU
+https://www.papaparse.com/docs#local-files
+https://nodejs.org/docs/latest/api/fs.html#fs_fs_writefile_file_data_options_callback
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/setSeconds
+https://www.papaparse.com/docs#unparse-config-default
--- fee-minimiser.js
+++ fee-minimiser.js
@@ -1,48 +1,117 @@
 const fs = require('fs');
 const papa = require('papaparse'); 
 
+const file = fs.createReadStream('customer-1234567-ledger.csv');//This will be the customer ledger used, change this file path to use a different csv.
+const newFile = "customer-1234567-ledger-new.csv";
+const transactions = []; // array to hold transaction objects
 
-const file = fs.createReadStream('customer-1234567-ledger.csv');//This will be the customer ledger used, change this file path to use a different csv. 
 var count = 0; // cache the running count
 
 //constructor for tranaction object
-function addTransaction(id, accType, initiator, date, tValue){
-	//create transaction record object
+function addTransaction(id, accType, initiator, date, tValue, pos){
+	//create transaction record object, pos represents the position the record shall be inserted
 	var record = {
 		'AccountID': id,
 		'AccountType' : accType,
 		'InitiatorType' : initiator,
-		'DateTime' : date,
-		'TransactionValue': tValue
+		'DateTime' : new Date(date),
+		'TransactionValue': parseFloat(tValue)
 	}
 	//add record
-	transactions.push(record);
-}
-
-const transactions = [];
-
-function minimizeFees(transactions){
-	var currentTotal = 0;
-	var savingsTotal = 0;
-	for(var i = transactions.length; i > 0; i--){
-		
+	if(pos == 0){
+		transactions.push(record);
 	}
+	else{
+		//insert at position
+		transactions.splice(pos, 0, record);
+	}
+	
 }
+
+//Parse the csv file into objects
 papa.parse(file, {
     header: true, //When header is set to true, results appear as an object not an array
     step: function(results, parser) {
         //add result as object to transactions list
-      	addTransaction(results.data[0].AccountID, results.data[0].AccountType, results.data[0].InitiatorType, results.data[0].DateTime, results.data[0].TransactionValue);
+      	addTransaction(results.data[0].AccountID, results.data[0].AccountType, results.data[0].InitiatorType, results.data[0].DateTime, results.data[0].TransactionValue, 0);
         count++;
         //console.log(transactions.length);
     },
     complete: function(results, file) {
-        console.log('parsing complete read', count, 'records before minimizing overdraft fees.');
-        //minimizeFees(transactions);
-
-        for(var i = transactions.length; i > 0; i--){
+        console.log('parsing complete read', count, 'records before minimising overdraft fees.');
+        for(var i = 0; i < transactions.length; i++){
         	console.log(transactions[i]);
         }
+        minimiseFees();
     }
 });
+
+// function to minimise overdraft fees 
+function minimiseFees(){
+	var currentTotal = 0.0;
+	var savingsTotal = 0.0;
+
+	console.log("------------------------------")
+	console.log("Minimising Overdraft Fees.....")
+	console.log("------------------------------")
+	for(var i = 0; i < transactions.length; i++){
+		if(transactions[i].AccountType == "CURRENT"){
+			console.log(transactions[i]);
+			//if the transactionValue is positive
+			if(Math.sign(transactions[i].TransactionValue) == 1){
+				currentTotal = transactions[i].TransactionValue + currentTotal; //add to total
+				console.log("current account total: ", currentTotal.toFixed(2));
+			 } 
+			 else{ //if negative
+			 	currentTotal = transactions[i].TransactionValue + currentTotal;
+			 	console.log("current account total: ", currentTotal.toFixed(2));
+			 	//if in overdraft ------ Overdraft minimising functionality -------
+			 	if(currentTotal < 0){
+			 		//if have enough savings
+					if(savingsTotal >= Math.abs(currentTotal)){
+						console.log("Transferring funds from savings...");
+						//get time of transaction and add on seconds to it
+						var dt = transactions[i].DateTime;
+						dt.setSeconds( dt.getSeconds() + 10 );
+						//add transaction - money from savings
+						addTransaction('123', 'SAVINGS', 'ACCOUNT-HOLDER', dt, currentTotal.toFixed(2), (i + 1));
+						dt.setSeconds( dt.getSeconds() + 20 );
+						//add transaction money - into current
+						addTransaction('789', 'CURRENT', 'ACCOUNT-HOLDER', dt, Math.abs(currentTotal).toFixed(2), (i + 2) );
+					}
+				}
+			 }
+		}
+		else if(transactions[i].AccountType == "SAVINGS") {
+			console.log(transactions[i]);
+			if(Math.sign(transactions[i].TransactionValue) == 1){
+				savingsTotal = transactions[i].TransactionValue + savingsTotal;
+				console.log("savings total: ", savingsTotal.toFixed(2));
+			 } 
+			 else{
+			 	savingsTotal = transactions[i].TransactionValue + savingsTotal;
+			 	console.log("savings total: ", savingsTotal.toFixed(2));
+			 }
+		}
+	}
+
+	console.log("parsing json to csv file");
+
+	//change objects to json
+	const stringTransactions = JSON.stringify(transactions);
+
+	//parse JSON to csv string
+	var csv = papa.unparse(stringTransactions, {
+		header: true,
+		quotes: true
+	});
+
+	//Write to file
+	fs.writeFile(newFile, csv, (err) => {
+	  if (err) throw err;
+	  console.log('The file has been saved!');
+	});
+
+}
+
 
